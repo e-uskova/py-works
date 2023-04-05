d2b731dfaa5dbd986ec98c5beea7cc75dc8b1edd
euskova <euskova@turbosolution.ru>
2023-04-05 16:50:43 +0300

    Вроде как работает

--- PAAiSD/PAAiSD1.py
+++ PAAiSD/PAAiSD1.py
@@ -1,7 +1,9 @@
 import os
 import numpy as np
 from datetime import datetime
-from multiprocessing import Process
+from multiprocess import Process, Manager
+# from multiprocessing import Process, Manager
+from multiprocessing import Pool, TimeoutError, current_process
 
 def printarr(arr):
     for row in arr:
@@ -9,73 +11,230 @@ def printarr(arr):
 
 def ad(arr, i, j):
     return np.delete(np.delete(np.copy(arr), (i), 0), (j), 1)
-        
-# def doubler(number):
-#     """
-#     Функция умножитель на два
-#     """
-#     result = number * 2
-#     proc = os.getpid()
-#     print('{0} doubled to {1} by process id: {2}'.format(
-#         number, result, proc))
+
+###############################################################################
+
+# def det4 (arr):
+#     res = []
+#     for i in range(4):
+#         res.append(arr[i][0] * det3(ad(arr, i, 0), i))
+#     return sum(res)
+
+# def det3 (arr, idx):
+#     res = []
+#     for i in range(3):
+#         res.append(arr[i][0] * det2(ad(arr, i, 0), i))
+#     return pow(-1, idx + 2) * sum(res)
+
+# def det2 (arr, idx):
+#     res = arr[0, 0] * arr[1, 1] - arr[1, 0] * arr[0, 1]
+#     return pow(-1, idx + 2) * res
+
+# ###############################################################################
+
+def det4p (arr):
+    res4 = Manager().list()
+    procs = []
+
+    for i in range(4):
+        args = (res4, ad(arr, i, 0), i, arr[i][0])
+        proc = Process(target=det3p, args=args) # розовый
+        proc.start()
+        procs.append(proc)
+
+    for proc in procs:
+        proc.join()
     
-def y1 (op1, op2) :
-    return op1 * op2
+    return sum(res4) # серый
 
-def y2 (op1, op2, op3) :
-    return op1 * (op2 - op3)
+def det3p (res, arr, idx, el):
+    res3 = Manager().list()
+    procs = []
 
-def y3 (op1, op2, op3, op4) :
-    return op1 * (op2 - op3 + op4)
-
-def y4 (op1, op2, op3, op4) :
-    return op1 - op2 + op3 - op4
-
-def calc (arr) :
-    for i in range(4):
-        for j in range(4):
-            pass
-
-def det4 (arr):
-    res = []
-    for i in range(4):
-        res.append(-pow(-1, i + 1) * arr[i][0] * det3(ad(arr, i, 0)))
-    return sum(res)
-
-def det3 (arr):
-    res = []
     for i in range(3):
-        res.append(pow(-1, i + 2) * arr[i][0] * det2(ad(arr, i, 0)))
+        args = (res3, ad(arr, i, 0), i, arr[i][0])
+        proc = Process(target=det2p, args=args) # голубой
+        proc.start()
+        procs.append(proc)
+
+    for proc in procs:
+        proc.join()
+
+    res.append(pow(-1, idx + 2) * el * sum(res3)) # фиолетовый
+        
+def det2p (res, arr, idx, el):  
+    res2 = Manager().list()
+
+    p1 = Process(target=mult, args=(res2, arr[0, 0], arr[1, 1], 0))
+    p1.start()
+    p2 = Process(target=mult, args=(res2, arr[0, 1], arr[1, 0], 1))
+    p2.start()
+
+    p1.join()
+    p2.join()
+
+    res.append(pow(-1, idx + 2) * el * sum(res2)) # зеленый
+
+def mult(res, el1, el2, idx):
+    res.append(pow(-1, idx + 2) * el1 * el2) # желтый
+    
+###############################################################################
+
+# def calc_det4o(arr):
+#     procs = []
+#     det3s = calc_det3o(arr)
+#     print(det3s)
+
+#     res = Manager().dict()
+#     args = [(m, k, i, j) for m in range(4) for k in range(4) for i in range(3) for j in range(i + 1, 4) if (k != m)&(i != k)&(j != k)&(i != m)&(j != m)]
+
+#     def det (m, k, i, j):
+#         res[(m, k, i, j)] = (-1) ** (m) * arr[m][0] * det3s[(k, i, j)]
+  
+#     for i in range(12):
+#         proc = Process(target=det, args=args[i])
+#         proc.start()
+#         procs.append(proc)
+
+#     for proc in procs:
+#         proc.join() 
+
+#     return res  
+
+# def calc_det3o(arr):
+#     procs = []
+#     det2s = calc_det2o(arr)
+
+#     res = Manager().dict()
+#     args = [(k, i, j) for k in range(4) for i in range(3) for j in range(i + 1, 4) if (i != k)&(j != k)]
+
+#     def det (k, i, j):
+#         res[(k, i, j)] = (-1) ** (k) * arr[k][1] * det2s[(i, j)]
+  
+#     for i in range(12):
+#         proc = Process(target=det, args=args[i])
+#         proc.start()
+#         procs.append(proc)
+
+#     for proc in procs:
+#         proc.join() 
+
+#     return res     
+
+# def calc_det2o(arr):
+#     procs = []
+#     mults = calc_mults(arr)
+
+#     res = Manager().dict()
+#     args = [(i, j) for i in range(3) for j in range(i + 1, 4)]
+
+#     def det (i, j):
+#         res[(i, j)] = mults[(i, j)] + mults[(j, i)]
+  
+#     for i in range(6):
+#         proc = Process(target=det, args=args[i])
+#         proc.start()
+#         procs.append(proc)
+
+#     for proc in procs:
+#         proc.join() 
+
+#     return res     
+
+
+# def calc_mults(arr):
+#     procs = []
+#     res = Manager().dict()
+#     args = [(i, j) for i in range(4) for j in range(4) if i != j]
+
+#     def mulo (i, j):
+#         res[(i, j)] = (-1) ** (int(i > j)) * arr[i][2] * arr[j][3]    
+
+#     for i in range(12):
+#         proc = Process(target=mulo, args=args[i])
+#         proc.start()
+#         procs.append(proc)
+
+#     for proc in procs:
+#         proc.join()
+
+#     return res
+
+###############################################################################
+
+def inv_count (m, k, i, j):
+    count = 0
+    if k < m : count += 1
+    if i < k : count += 1
+    if i < m : count += 1
+    if j < i : count += 1
+    if j < k : count += 1
+    if j < m : count += 1
+    return count
+
+###############################################################################
+
+def determ4 (arr):
+    res = []
+    args = [(m, k, i, j) for m in range(4) for k in range(4) for i in range(4) for j in range(4) if (k != m)&(i != k)&(j != k)&(i != m)&(j != m)&(i != j)]
+
+    def mult(m, k, i, j):
+        return arr[m][0] * arr[k][1] * arr[i][2] * arr[j][3] * (-1) ** (inv_count (m, k, i, j))
+
+    for i in range(24):
+        res.append(mult(*args[i]))
+
     return sum(res)
 
-def det2 (arr):
-    res = arr[0, 0] * arr[1, 1] - arr[1, 0] * arr[0, 1]
-    return res
+###############################################################################
+
+def determ4p (arr):
+    procs = []
+    res = Manager().list()
+    args = [(m, k, i, j) for m in range(4) for k in range(4) for i in range(4) for j in range(4) if (k != m)&(i != k)&(j != k)&(i != m)&(j != m)&(i != j)]
+
+    def mult(m, k, i, j):
+        res.append(arr[m][0] * arr[k][1] * arr[i][2] * arr[j][3] * (-1) ** (inv_count (m, k, i, j)))
+
+    for i in range(24):
+        proc = Process(target=mult, args=args[i])
+        proc.start()
+        procs.append(proc)
+
+    for proc in procs:
+        proc.join()
+
+    return sum(res)
+
+###############################################################################
 
 if __name__ == '__main__':
-    # numbers = [5, 10, 15, 20, 25]
-    # procs = []
 
-    # for index, number in enumerate(numbers):
-    #     proc = Process(target=doubler, args=(number,))
-    #     procs.append(proc)
-    #     proc.start()
-
-    # for proc in procs:
-    #     proc.join()
     arr = np.array([[1, 2, 3, 4],
                     [5, 7, 9, 11],
                     [9, 3, 9, 8],
                     [7, 8, 5, 4]])
-    
+
     # det = 12
 
     # printarr(arr)
 
     # time_start = datetime.now()
-    det = det4(arr)
+    # det = det4(arr)
     # time_calc = datetime.now() - time_start
-    print(det)
+    # print(det)
     # print(time_calc)
 
+    time_start = datetime.now()
+    det = determ4p(arr)
+    time_calc = datetime.now() - time_start
+    print(det)
+    print(time_calc)
 
+    # time_start = datetime.now()
+    # det = np.linalg.det(arr)
+    # time_calc = datetime.now() - time_start
+    # print(det)
+    # print(time_calc)
+
+    # print(inv_count(1, 2, 3))
